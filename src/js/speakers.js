e9309dc692ff4606202215f7fa0269f3761d150d
Yamelnyk <yachkana@ukr.net>
2025-04-11 10:41:49 +0300

    add speaker section

---
+++ src/js/speakers.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
+  const dropDownBtn = dropDownWrapper.querySelector('.dropdown-button');
+  const dropDownList = dropDownWrapper.querySelector('.dropdown-list');
+  const dropDownListItems = dropDownList.querySelectorAll(
+    '.dropdown-list-item'
+  );
+  const dropDownInput = dropDownWrapper.querySelector('.dropdown-input-hidden');
+
+  dropDownBtn.addEventListener('click', function () {
+    dropDownList.classList.toggle('dropdown-list-visible');
+    dropDownWrapper.classList.toggle('is-open');
+  });
+
+  dropDownListItems.forEach(function (listItem) {
+    listItem.addEventListener('click', function (e) {
+      e.stopPropagation();
+      dropDownBtn.innerText = this.innerText;
+      dropDownInput.value = this.dataset.value;
+      dropDownList.classList.remove('dropdown-list-visible');
+    });
+  });
+
+  document.addEventListener('click', function (e) {
+    if (e.target !== dropDownBtn) {
+      dropDownList.classList.remove('dropdown-list-visible');
+    }
+  });
+});
