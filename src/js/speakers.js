e6a6b5db96b8abf0ace9addd7ba7b34d0c6c1a43
Yamelnyk <yachkana@ukr.net>
2025-04-14 12:17:36 +0300

    add load more speakers

--- src/js/speakers.js
+++ src/js/speakers.js
@@ -1,10 +1,15 @@
+const speakerItems = document.querySelectorAll('.speaker-item');
+const loadmoreBtn = document.querySelector('.see-more-tab');
+let currentItems = 9;
+
+let activeClass = 'speaker-item';
+
 document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
   const dropDownBtn = dropDownWrapper.querySelector('.dropdown-button');
   const dropDownList = dropDownWrapper.querySelector('.dropdown-list');
   const dropDownListItems = dropDownList.querySelectorAll(
     '.dropdown-list-item'
   );
-  const dropDownInput = dropDownWrapper.querySelector('.dropdown-input-hidden');
 
   dropDownBtn.addEventListener('click', function () {
     dropDownList.classList.toggle('dropdown-list-visible');
@@ -15,62 +20,101 @@ document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
     listItem.addEventListener('click', function (e) {
       e.stopPropagation();
       dropDownBtn.innerText = this.innerText;
-      dropDownInput.value = this.dataset.value;
       dropDownList.classList.remove('dropdown-list-visible');
-      dropDownWrapper.classList.toggle('is-open');
+      dropDownWrapper.classList.remove('is-open');
+      filterSpeakersByCategory(this.dataset.value);
     });
   });
 
   document.addEventListener('click', function (e) {
     if (e.target !== dropDownBtn) {
       dropDownList.classList.remove('dropdown-list-visible');
-      dropDownWrapper.classList.toggle('is-open');
+      dropDownWrapper.classList.remove('is-open');
     }
   });
 });
 
 const expertsTabList = document.querySelector('.experts-items');
-const speakerItems = document.querySelectorAll('.speaker-item');
 const tabItems = expertsTabList.querySelectorAll('.experts-item');
 
-function filter() {
+function filterSpeakerDesk() {
   expertsTabList.addEventListener('click', event => {
-    const targetId = event.target.dataset.id;
     const target = event.target;
+    const targetId = target.dataset.id;
 
     if (target.classList.contains('experts-item')) {
       tabItems.forEach(tabItem => tabItem.classList.remove('is-active'));
       target.classList.add('is-active');
-    }
-
-    switch (targetId) {
-      case 'all':
-        getItems('speaker-item');
-        break;
-      case 'digital':
-        getItems('digital-artist');
-        break;
-      case 'nft':
-        getItems('nft-enthusiast');
-        break;
-      case 'fashion':
-        getItems('fashion-designer');
-
-        break;
-      case 'influencers':
-        getItems('influencer');
-        break;
+      filterSpeakersByCategory(targetId);
     }
   });
 }
-filter();
+filterSpeakerDesk();
+
+function filterSpeakersByCategory(category) {
+  switch (category) {
+    case 'all':
+      activeClass = 'speaker-item';
+      break;
+    case 'digital':
+      activeClass = 'digital-artist';
+      break;
+    case 'nft':
+      activeClass = 'nft-enthusiast';
+      break;
+    case 'fashion':
+      activeClass = 'fashion-designer';
+      break;
+    case 'influencers':
+      activeClass = 'influencer';
+      break;
+  }
+  currentItems = 9;
+  getItems(activeClass);
+}
 
 function getItems(className) {
+  let count = 0;
   speakerItems.forEach(item => {
-    if (item.classList.contains(className)) {
-      item.style.display = 'block';
+    if (className === 'speaker-item' || item.classList.contains(className)) {
+      count++;
+      item.style.display = count <= currentItems ? 'inline-block' : 'none';
     } else {
       item.style.display = 'none';
     }
   });
+  handleLoadMoreVisibility();
 }
+
+function handleLoadMoreVisibility() {
+  const matchingItems = [...speakerItems].filter(
+    item =>
+      item.classList.contains(activeClass) || activeClass === 'speaker-item'
+  );
+
+  if (currentItems >= matchingItems.length) {
+    loadmoreBtn.style.display = 'none';
+  } else {
+    loadmoreBtn.style.display = 'inline-block';
+  }
+}
+
+if (loadmoreBtn) {
+  loadmoreBtn.addEventListener('click', () => {
+    const matchingItems = [...speakerItems].filter(
+      item =>
+        item.classList.contains(activeClass) || activeClass === 'speaker-item'
+    );
+
+    const nextItems = currentItems + 3;
+
+    for (let i = currentItems; i < nextItems && i < matchingItems.length; i++) {
+      matchingItems[i].style.display = 'inline-block';
+    }
+
+    currentItems += 3;
+    handleLoadMoreVisibility();
+  });
+}
+
+getItems(activeClass);
